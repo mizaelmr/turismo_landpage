5df45b545f3da223bd09778ef2b233cad7f5ef84
mizaelmr <mizaelmr@gmail.com>
2025-04-11 14:54:17 -0300

    update css

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -6,7 +6,7 @@ interface ContainerProps {
 
 const Container: React.FC<ContainerProps> = ({ children }) => {
   return (
-    <div className="flex justify-center items-center max-w-[1224px] mx-auto space-x-24 py-12 lg:py-6 lg:justify-start">
+    <div className="flex flex-col justify-center items-center max-w-[1224px] mx-auto space-x-24 py-12 lg:py-6 lg:justify-start">
       {children}
     </div>
   );
