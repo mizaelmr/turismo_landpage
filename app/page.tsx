82c06e6c688169ecceee0e5ce834a9fa8a8d51e6
Nicollas Ryan <nbarretoduarte@gmail.com>
2025-09-18 14:30:00 -0300

    feat: add bus image

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import { Feedback } from "./_sessions/feedback";
-import { Financial } from "./_sessions/financial";
 import { Footer } from "./_sessions/footer";
 import { Hero } from "./_sessions/hero";
-import { Pag2 } from "./_sessions/pag2";
 import { Plans } from "./_sessions/plans";
 import { Questions } from "./_sessions/questions";
 import { Resources } from "./_sessions/resources";
